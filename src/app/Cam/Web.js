82a7f35d8abfef56b15d07cdcf748a704885b665
Naitik <naitikraj12935@gmail.com>
2024-04-05 08:38:53 +0530

    'add rear camera'

--- src/app/Cam/Web.js
+++ src/app/Cam/Web.js
@@ -10,10 +10,13 @@ export const CustomWebcam = () => {
         setImgSrc(img);
     }, [camRef]);
     
-    const ResetCapture=()=>{
-        setImgSrc(null)
-    }
+    const ResetCapture = () => {
+        setImgSrc(null);
+    };
 
+    const videoConstraints = {
+        facingMode: "environment", // Switches to back camera
+    };
 
     return (
         <div className="flex flex-col mt-20 items-center">
@@ -22,22 +25,25 @@ export const CustomWebcam = () => {
                 height={400}
                 width={400}
                 ref={camRef}
+                videoConstraints={videoConstraints}
             />
-            {
-               imgSrc===null ? (
-                    <button onClick={capture} className="bg-slate-500 mt-4 py-2 px-4 rounded-lg text-white">
-                Take picture
-            </button>
-                ):(
-                    <button onClick={ResetCapture} className="bg-slate-500 mt-4 py-2 px-4 rounded-lg text-white">
-                Reset picture
-            </button>
-                )
-            }
-            
-            {imgSrc && (
-                <img src={imgSrc} className="mt-4" alt="Captured" />
+            {imgSrc === null ? (
+                <button
+                    onClick={capture}
+                    className="bg-slate-500 mt-4 py-2 px-4 rounded-lg text-white"
+                >
+                    Take picture
+                </button>
+            ) : (
+                <button
+                    onClick={ResetCapture}
+                    className="bg-slate-500 mt-4 py-2 px-4 rounded-lg text-white"
+                >
+                    Reset picture
+                </button>
             )}
+
+            {imgSrc && <img src={imgSrc} className="mt-4" alt="Captured" />}
         </div>
     );
 };
